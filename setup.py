d19fa91690e787264f5ecbe1ea622bbd65293c81
justanz <Justin@justanz.com>
2023-07-22 14:58:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """Python Carson Living setup script."""
 from setuptools import setup
 
-_VERSION = '0.0.6'
+_VERSION = '0.0.8'
 
 
 def readme():
@@ -12,7 +12,7 @@ def readme():
 
 
 setup(
-    name='c_l_test',
+    name='c_l_testing',
     packages=['carson_living'],
     version=_VERSION,
     description='A Python library to communicate with'
